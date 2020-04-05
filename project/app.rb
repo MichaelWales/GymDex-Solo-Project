1560173e4289594c328e6698a485dee079da0a81
Michael Wales <michaelwales@gmail.com>
2020-04-05 14:21:12 +0100

    Adds Customer Index

--- project/app.rb
+++ project/app.rb
@@ -1,8 +1,10 @@
 require('sinatra')
-require('sinatra/contrib/all')
+require('sinatra/contrib/all') if development?
 require_relative('controllers/bookings_controller')
 require_relative('controllers/customers_controller')
 require_relative('controllers/sessions_controller')
+also_reload("./models/*")
+
 
 get '/' do
   erb(:index)
--- project/controllers/customers_controller.rb
+++ project/controllers/customers_controller.rb
@@ -1,15 +1,10 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require_relative('views/customers/new')
-require_relative('views/customers/show')
+require_relative('../models/customer')
 also_reload( '../models/*' )
 
 
 get '/customer/all' do
   @customers = Customer.all_members()
   erb(:"customers/index")
-end
-
-get '/customer/new' do
-  erb(:new)
 end
