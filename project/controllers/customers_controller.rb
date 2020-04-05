b3046b176094aa3e1731ab8525fe28dd012459aa
Michael Wales <michaelwales@gmail.com>
2020-04-05 15:07:17 +0100

    Adds Index for Sessions

--- project/controllers/customers_controller.rb
+++ project/controllers/customers_controller.rb
@@ -3,7 +3,6 @@ require('sinatra/contrib/all') if development?
 require_relative('../models/customer')
 also_reload( '../models/*' )
 
-
 get '/customer/all' do
   @customers = Customer.all_members()
   erb(:"customers/index")
--- project/controllers/sessions_controller.rb
+++ project/controllers/sessions_controller.rb
@@ -2,3 +2,8 @@ require('sinatra')
 require('sinatra/contrib/all') if development?
 require_relative('../models/session')
 also_reload( '../models/*' )
+
+get '/sessions/all' do
+  @sessions = Session.all_sessions()
+  erb(:"sessions/index")
+end
